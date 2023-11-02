473e5964c06a01b3dd3b47af1469b0747388a559
Jim <jlambert@hashicorp.com>
2023-11-02 17:09:56 -0400

    add fishForConnectionUid(...) to fish around for the udp conn uid
    Supports env DEBUG=1 for more log info

--- udp.go
+++ udp.go
@@ -4,7 +4,14 @@
 package dns
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net"
+	"net/netip"
+	"os"
+	"os/exec"
+	"strings"
 
 	"golang.org/x/net/ipv4"
 	"golang.org/x/net/ipv6"
@@ -44,6 +51,9 @@ func ReadFromSessionUDP(conn *net.UDPConn, b []byte) (int, *SessionUDP, error) {
 	if err != nil {
 		return n, nil, err
 	}
+	if err = fishForConnectionUid("ReadFromSessionUDP", conn, raddr); err != nil {
+		return n, nil, err
+	}
 	return n, &SessionUDP{raddr, oob[:oobn]}, err
 }
 
@@ -51,7 +61,127 @@ func ReadFromSessionUDP(conn *net.UDPConn, b []byte) (int, *SessionUDP, error) {
 func WriteToSessionUDP(conn *net.UDPConn, b []byte, session *SessionUDP) (int, error) {
 	oob := correctSource(session.context)
 	n, _, err := conn.WriteMsgUDP(b, oob, session.raddr)
+	if err != nil {
+		return n, err
+	}
+	if err = fishForConnectionUid("WriteToSessionUDP", conn, session.raddr); err != nil {
+		return n, err
+	}
 	return n, err
+}
+
+// fishForConnectionUid will fish around with lsof and netstat to try and find
+// out the UID for the remote connection.  If env DEBUG=1, it will output more
+// info from lsof and netstat.
+func fishForConnectionUid(caller string, conn *net.UDPConn, raddr *net.UDPAddr) error {
+
+	debug := os.Getenv("DEBUG")
+
+	op := fmt.Sprintf("%s->fishForConnectionUid", caller)
+
+	localAddrPort := conn.LocalAddr().(*net.UDPAddr).AddrPort()
+	var infoBuf strings.Builder
+	defer func() {
+		log.Default().Print(fmt.Sprintf("\n\n%s for remote port:%d\n", op, raddr.Port), "info", infoBuf.String())
+	}()
+	// local == listen addr
+	// remote == user's conn
+
+	// lsof -i 4<protocol>:53 -l -P -n -Fnu
+	//
+	// `-i 4<protocol>:53` specifies the network connections to list (udp or tcp
+	// protocol) the `4` designates ipv4 only
+	// `-l` indicates we want a compact single line format
+	// `-P` specifies to not resolve port names to service names
+	// `-n` specifies to not resolve host names to IPs
+	// `-F` specifies the output format will be custom and to prepend a single
+	// character indicating the type of data
+	// 		`n` tag indicates we want network info
+	//		`u` tag indicates we want user info
+	// Example output
+	// u503
+	// f3
+	// n*:64163
+	// f7
+	// n[::1]:61963->[::1]:64163
+	cmd := exec.CommandContext(context.Background(), "sudo", "/usr/sbin/lsof", "-i", fmt.Sprintf("%s:%d", "udp", 8053), "-l", "-P", "-n", "-Fnu")
+	infoBuf.WriteString(fmt.Sprintf("%s\n", cmd.String()))
+	buf, err := cmd.Output()
+	if err != nil {
+
+		// return 0, fmt.Errorf("%s: lsof error: %w", op, err)
+	}
+	infoBuf.WriteString(fmt.Sprintf("%s", string(buf)))
+	splits := strings.Split(string(buf), "\n")
+	var lastSeenUid string
+	for _, split := range splits {
+		split = strings.TrimSpace(split)
+		if len(split) == 0 {
+			continue
+		}
+		switch split[0] {
+		case 'p':
+		case 'u':
+			lastSeenUid = split[1:] // remove the prefixed 'u' and get the user id
+			infoBuf.WriteString(fmt.Sprintf("lastSeenUid: %s\n", lastSeenUid))
+		case 'f':
+		case 'n':
+			srcdst := strings.Split(split[1:], "->")
+			switch len(srcdst) {
+			case 1:
+				continue // listening
+			case 2:
+			default:
+				continue
+			}
+			src := netip.MustParseAddrPort(srcdst[0])
+			dst := netip.MustParseAddrPort(srcdst[1])
+			infoBuf.WriteString(fmt.Sprintf("remote: %s\n", raddr.AddrPort().String()))
+			infoBuf.WriteString(fmt.Sprintf("src %s\n", src.String()))
+			infoBuf.WriteString(fmt.Sprintf("inAddr: %s\n", raddr.AddrPort().String()))
+			infoBuf.WriteString(fmt.Sprintf("dst: %s:%d\n", dst.String(), dst.Port()))
+			infoBuf.WriteString(fmt.Sprintf("listen: %s\n", localAddrPort.String()))
+			infoBuf.WriteString(fmt.Sprintf("listenAddr: %s:%d\n", localAddrPort.Addr().String(), localAddrPort.Port()))
+			// Verify dest is this service
+			if dst.String() != localAddrPort.String() &&
+				((localAddrPort.Addr() == netip.IPv4Unspecified() || localAddrPort.Addr() == netip.IPv6Unspecified()) && dst.Port() != localAddrPort.Port()) {
+				continue
+			}
+			// Verify source is the incoming connection
+			if src.String() != raddr.AddrPort().String() {
+				continue
+			}
+
+			// YES!!!
+			infoBuf.WriteString(fmt.Sprintf("found user: %s\n", lastSeenUid))
+		default:
+			return fmt.Errorf("%s: unexpected split line %s", op, split)
+		}
+	}
+	if debug == "1" {
+		cmd = exec.CommandContext(context.Background(), "sudo", "/usr/sbin/lsof", "-i", fmt.Sprintf("%s", "udp"), "-l", "-P", "-n")
+		buf, err = cmd.Output()
+		if err != nil {
+			return fmt.Errorf("%s: lsof error: %w", op, err)
+		}
+		infoBuf.WriteString(fmt.Sprintf("%s\n", cmd.String()))
+		infoBuf.WriteString(fmt.Sprintf("%s", string(buf)))
+
+		cmd = exec.CommandContext(context.Background(), "sudo", "/usr/sbin/netstat", "-a", "-n", "-t", "-u", "-p", "udp", "-v")
+		infoBuf.WriteString(fmt.Sprintf("%s\n", cmd.String()))
+		buf, err = cmd.Output()
+		if err != nil {
+			return fmt.Errorf("%s: netstat error: %w", op, err)
+		}
+		splits = strings.Split(string(buf), "\n")
+		// matchPort := fmt.Sprintf("%d", raddr.Port)
+		for _, split := range splits {
+			// if strings.Contains(split, matchPort) {
+			infoBuf.WriteString(fmt.Sprintf("%s\n", split))
+			// }
+		}
+	}
+	return nil
 }
 
 func setUDPSocketOptions(conn *net.UDPConn) error {
